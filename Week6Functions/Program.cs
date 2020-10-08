8f7bc76a586e38b001d1f8705b3b285411a96857
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:26:47 +0300

    first commit

---
+++ Week6Functions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Dynamic;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call a function
+            HelloUser();
+            Sum();
+        }
+
+        public static void HelloUser() //create a function
+        {
+            Console.WriteLine("Enter your name");
+            string UserName = Console.ReadLine();
+            Console.WriteLine($"Hello, {UserName}");
+        }
+
+        public static void Sum()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int o = Int32.Parse(Console.ReadLine());
+
+            int result = a + o;
+            Console.WriteLine(result);
+        }
+    }
+}
